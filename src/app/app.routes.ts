c3384ae4e20f51f8ad6386f5a2c1f94ff433f130
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-06-07 20:49:42 -0300

    Feat: Cria rota para consulta e cadastro.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Consulta } from './consulta/consulta';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro', component: Cadastro },
+    { path: 'consulta', component: Consulta}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
