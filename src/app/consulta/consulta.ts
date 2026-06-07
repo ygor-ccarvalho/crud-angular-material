8e82b25b6d6c8ace28bfa113bc10b035f58ecfce
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-06-07 20:49:00 -0300

    Feat: Cria o componente consulta.

---
+++ src/app/consulta/consulta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta',
+  imports: [],
+  templateUrl: './consulta.html',
+  styleUrl: './consulta.scss',
+})
+export class Consulta {}
