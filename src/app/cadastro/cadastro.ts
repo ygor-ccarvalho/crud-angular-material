514174ea53f3b430509ac1943d31a0462a37c44e
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-06-07 20:48:34 -0300

    Feat: Cria o componente Cadastro.

---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss',
+})
+export class Cadastro {}
