c668b95f89e285f01e61b4337092ee24f73f2dd8
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-06-11 10:42:50 -0300

    Feat: Adiciona a biblioteca NGX-Mask

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,39 +1,45 @@
 import { Component } from '@angular/core';
-import {  FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cadastro',
-  imports:
-    [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatButtonModule],
-  
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatButtonModule, RouterLink],
+
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
-  
+
 
 export class Cadastro {
 
   form = new FormGroup({
-    nome: new FormControl('', Validators.required),
+
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+
     email: new FormControl('', [
       Validators.required,
       Validators.email
     ]),
-    cpf: new FormControl('', Validators.required), 
+
+    cpf: new FormControl('', Validators.required),
     telefone: new FormControl('', Validators.required),
-    sexo: new FormControl('', Validators.required),
-    nascimento: new FormControl('', Validators.required),
+    sexo: new FormControl(''),
+    nascimento: new FormControl(''),
     cep: new FormControl('', Validators.required),
     endereco: new FormControl('', Validators.required),
     numero: new FormControl('', Validators.required),
-    complemento: new FormControl('', Validators.required),
+    complemento: new FormControl(''),
     cidade: new FormControl('', Validators.required),
-    estado: new FormControl('', Validators.required)    
+    estado: new FormControl('', Validators.required)
   });
 
   salvar() {
